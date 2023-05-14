da0df2b63fbb41f9c460359ad8283f20576ef7a0
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-05-14 18:32:02 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -8,6 +8,6 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Class that defines attributes for amenity """
+    """A Class that defines attributes for amenity """
 
     name = ""
