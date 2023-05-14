0515eac70754ae774e809da83b78cf149e0167d6
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-05-14 18:13:51 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,6 +31,25 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(dictionary, f)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """
         deserializes the JSON file to __objects (only if the JSON file
@@ -39,18 +58,10 @@ class FileStorage:
         """
         try:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                from models.base_model import BaseModel
-                from models.user import User
-                from models.city import City
-                from models.amenity import Amenity
-                from models.place import Place
-                from models.review import Review
-                from models.state import State
 
                 dictionary = json.load(f)
-                for key, value in dictionary.items():
-                    class_name = value['__class__']
-                    FileStorage.__objects[key] = eval(class_name)(**value)
+                dictionary = {key: self.classes()[value["__class__"]](**value)
+                              for key, value in dictionary.items()}
+                FileStorage.__objects = dictionary
         except FileNotFoundError:
             pass
-
