767340d52c71bd7a289697ccfc44d7f4dfaa03f4
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-05-24 16:18:07 -0700

    advance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,13 @@ storage of instances
 """
 
 import json
+from models.base_model import BaseModel  # noqa
+from models.user import User  # noqa
+from models.state import State  # noqa
+from models.city import City  # noqa
+from models.place import Place  # noqa
+from models.amenity import Amenity  # noqa
+from models.review import Review  # noqa
 
 
 class FileStorage:
@@ -20,36 +27,15 @@ class FileStorage:
 
     def new(self, obj):
         """ Sets in __objects the obj key <obj class name>.id """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        key_obj = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key_obj] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
-        dictionary = {}
-        for key, value in FileStorage.__objects.items():
-            dictionary[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(dictionary, f)
-
-    @classmethod
-    def classes(cls):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
+        dict_obj = FileStorage.__objects
+        dic_obj = {obj: dict_obj[obj].to_dict() for obj in dict_obj.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(dic_obj, f)
 
     def reload(self):
         """
@@ -58,11 +44,11 @@ class FileStorage:
         doesn’t exist, no exception should be raised)
         """
         try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-
-                dictionary = json.load(f)
-                dictionary = {key: self.classes()[value["__class__"]](**value)
-                              for key, value in dictionary.items()}
-                FileStorage.__objects = dictionary
+            with open(FileStorage.__file_path) as f:
+                dic_obj = json.load(f)
+                for obj_data in dic_obj.values():
+                    class_name = obj_data["__class__"]
+                    del obj_data["__class__"]
+                    self.new(eval(class_name)(**obj_data))
         except FileNotFoundError:
-            pass
+            return
