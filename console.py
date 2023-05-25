6c38a03e7a1fee5269270328f4522c3effa11e25
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-05-24 19:28:48 -0700

    commit

--- console.py
+++ console.py
@@ -16,7 +16,6 @@ from models.review import Review  # noqa
 
 
 def parse(arg):
-
     curly_braces_match = re.search(r"\{(.*?)\}", arg)
 
     brackets_match = re.search(r"\[(.*?)\]", arg)
@@ -65,11 +64,12 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """EOF command exits program"""
+        """EOF signal to exit the program."""
+        print("")
         return True
 
     def emptyline(self):
@@ -104,7 +104,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Usage: create <class>
-        Create a new class instance of BaseModel and print its id.
+        Create a new class instance and print its id.
         """
         argl = parse(arg)
         if len(argl) == 0:
@@ -116,8 +116,9 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
         """
-        Prints the str rep of an instance based on the class name and id"""
         argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
@@ -132,8 +133,8 @@ class HBNBCommand(cmd.Cmd):
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id
-        """
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id."""
         argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
@@ -149,8 +150,9 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_all(self, arg):
-        """Prints all string representation of all instances
-            based or not on the class name.
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects.
         """
         argl = parse(arg)
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
@@ -165,7 +167,8 @@ class HBNBCommand(cmd.Cmd):
             print(objl)
 
     def do_count(self, arg):
-        """Retrieve the number of instances of a given class."""
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
         argument = parse(arg)
         count = 0
         for obj in storage.all().values():
@@ -174,9 +177,11 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id
-            by adding or updating attribute
-        """
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
         argl = parse(arg)
         objdict = storage.all()
 
